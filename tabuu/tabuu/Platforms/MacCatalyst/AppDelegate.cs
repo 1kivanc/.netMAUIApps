54f7551af8b977f5cc64dd08661fb338247b7db8
1kivanc <kivanc0556@gmail.com>
2024-10-19 23:56:15 +0300

    tabu starter file

---
+++ tabuu/tabuu/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace tabuu
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
